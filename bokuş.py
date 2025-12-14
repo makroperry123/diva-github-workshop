f2fb73d8bbc79c94e258b0bd208636489ee5f9ad
Hamarat19 <mhamarat2001@gmail.com>
2025-12-14 17:29:02 +0300

    Please provide the file changes (diff) to generate a commit message.

--- bokuş.py
+++ bokuş.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # Basit bir Python programı
-
+#MELİH UÇMA
 def merhaba():
+    """OROPSU UTKU"""
     """Merhaba mesajı yazdır"""
     print("Merhaba, Dünya!")
     print("Bu basit bir Python programıdır.")
